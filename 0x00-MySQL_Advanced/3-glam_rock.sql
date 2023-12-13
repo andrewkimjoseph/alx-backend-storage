502e8a7fed34e70595715f33d231cf120587360e
Andrew Kim <andrewkimjoseph@gmail.com>
2023-12-13 21:03:19 +0300

    Committing local changes

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,10 @@
--- Lists all bands with Glam rock as their style,
--- ranked by their longevity
--- Column names must be: band_name & lifespan
+-- Import the metal_bands table dump before executing this script
 
-SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
+-- Lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+-- Column names must be: band_name and lifespan (in years until 2022)
+
+SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC
+ORDER BY lifespan DESC;
