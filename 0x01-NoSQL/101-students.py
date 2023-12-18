a02b498312ef53a132a188d0b59932281869bad0
Andrew Kim <andrewkimjoseph@gmail.com>
2023-12-18 23:08:21 +0300

    Committing local changes

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,4 +4,19 @@
 
 def top_students(mongo_collection):
     """ Returns all students sorted by average score"""
-    return list(mongo_collection.find())
+    # Retrieve all students from the collection
+    all_students = list(mongo_collection.find())
+
+    # Calculate the average score for each student
+    for student in all_students:
+        scores = student.get('scores', [])
+        if scores:
+            average_score = sum(score['score'] for score in scores) / len(scores)
+            student['averageScore'] = average_score
+        else:
+            student['averageScore'] = 0  # Set default average score if no scores are available
+
+    # Sort students based on average scores in descending order
+    sorted_students = sorted(all_students, key=lambda x: x['averageScore'], reverse=True)
+
+    return sorted_students
